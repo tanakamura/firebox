178aa2521ae30543dd42fae5177d64990fd4a075
tanakmura <tanakmura>
2022-10-13 00:23:42 +0900

    tmp

--- tools/rinit/src/main.rs
+++ tools/rinit/src/main.rs
@@ -1,21 +1,30 @@
-use libc::{getpid, mount, mkdir, system, open, setsid, ioctl, TIOCSCTTY, setenv,
-           O_RDWR, dup2, close, fork, execlp, execvp, _exit, EXIT_FAILURE, c_char, umount, sync, RB_POWER_OFF, reboot, EXIT_SUCCESS, waitpid,
-           sigfillset, sigset_t, sigprocmask, SIG_SETMASK, pid_t, siginfo_t, sigwaitinfo,
-           CLD_EXITED, CLD_KILLED, CLD_DUMPED, c_int, SIGCHLD, SIGUSR1, sleep, puts};
-use std::fs::File;
-use std::io::{BufReader, BufRead};
+use libc::{
+    _exit, c_char, c_int, close, dup2, execlp, execvp, fork, getpid, ioctl, mkdir, mount, open,
+    pid_t, reboot, setenv, setsid, sigfillset, siginfo_t, sigprocmask, sigset_t, sigwaitinfo,
+    sleep, sync, system, umount, waitpid, CLD_DUMPED, CLD_EXITED, CLD_KILLED, EXIT_FAILURE,
+    EXIT_SUCCESS, O_RDWR, RB_POWER_OFF, SIGCHLD, SIGUSR1, SIG_SETMASK, TIOCSCTTY,
+};
 use std::ffi::CString;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::mem;
 
 mod server;
 
 macro_rules! cs {
     ($lit:expr) => {
-        std::ffi::CStr::from_ptr(concat!($lit, "\0").as_ptr() as *const std::os::raw::c_char).as_ptr()
-    }
+        std::ffi::CStr::from_ptr(concat!($lit, "\0").as_ptr() as *const std::os::raw::c_char)
+            .as_ptr()
+    };
 }
 
-unsafe fn run_spawner(orig_sig : *const sigset_t, true_init: bool) -> std::io::Result<pid_t> {
+enum Mode {}
+
+struct State {
+    mode: Mode,
+}
+
+unsafe fn run_spawner(orig_sig: *const sigset_t, true_init: bool) -> std::io::Result<pid_t> {
     let pid = fork();
     if pid > 0 {
         return Ok(pid);
@@ -33,7 +42,7 @@ unsafe fn run_spawner(orig_sig : *const sigset_t, true_init: bool) -> std::io::R
 
             if let Ok(len) = len {
                 if len > 0 {
-                    line.truncate(len-1);
+                    line.truncate(len - 1);
                     let ttydev = CString::new("/dev/".to_owned() + &line).unwrap();
                     let fd = open(ttydev.as_ptr(), O_RDWR);
 
@@ -56,17 +65,17 @@ unsafe fn run_spawner(orig_sig : *const sigset_t, true_init: bool) -> std::io::R
         assert_eq!(system(cs!("dbus-daemon --system")), 0);
     }
 
-    unsafe fn spawn_commands(coms: Vec< Vec<&str>>, orig_sig:*const sigset_t ) {
+    unsafe fn spawn_commands(coms: Vec<Vec<&str>>, orig_sig: *const sigset_t) {
         for c in &coms {
             let mut vec = Vec::new();
             let mut ptrvec = Vec::new();
             for s in c {
                 let c = CString::new(*s);
-                ptrvec.push( c.as_ref().unwrap().as_ptr() );
+                ptrvec.push(c.as_ref().unwrap().as_ptr());
                 vec.push(c);
             }
 
-            ptrvec.push( std::ptr::null() );
+            ptrvec.push(std::ptr::null());
 
             let pid = fork();
             if pid > 0 {
@@ -80,35 +89,40 @@ unsafe fn run_spawner(orig_sig : *const sigset_t, true_init: bool) -> std::io::R
         }
     }
 
-    let coms = vec![vec!["seatd-launch", "--", "dbus-launch", "Hyprland", "--i-am-really-stupid"],
-                    vec!["/usr/libexec/iwd"],
-                    vec!["dhcpcd", "eth0"],
+    let coms = vec![
+        vec![
+            "seatd-launch",
+            "--",
+            "dbus-launch",
+            "Hyprland",
+            "--i-am-really-stupid",
+        ],
+        vec!["/usr/libexec/iwd"],
+        vec!["dhcpcd", "eth0"],
     ];
 
-    spawn_commands(
-        coms, orig_sig
-    );
+    spawn_commands(coms, orig_sig);
 
-//    let winsys_pid = fork();
-//    if winsys_pid == 0 {
-//        sigprocmask(SIG_SETMASK, orig_sig, std::ptr::null_mut());
-//        execlp(cs!("seatd-launch"), cs!("seatd-launch"), cs!("--"),
-//               cs!("dbus-launch"),
-//               cs!("Hyprland"), cs!("--i-am-really-stupid"),
-//               //cs!("weston"),
-//               std::ptr::null::<*const i8>());
-//        _exit(EXIT_FAILURE);
-//    }
-//    assert_ne!(winsys_pid, -1);
-//
-//    let iwd_pid = fork();
-//    if iwd_pid == 0 {
-//        sigprocmask(SIG_SETMASK, orig_sig, std::ptr::null_mut());
-//        execlp(cs!("/usr/libexec/iwd"), cs!("/usr/libexec/iwd"),
-//               std::ptr::null::<*const i8>());
-//        _exit(EXIT_FAILURE);
-//    }
-//    assert_ne!(iwd_pid, -1);
+    //    let winsys_pid = fork();
+    //    if winsys_pid == 0 {
+    //        sigprocmask(SIG_SETMASK, orig_sig, std::ptr::null_mut());
+    //        execlp(cs!("seatd-launch"), cs!("seatd-launch"), cs!("--"),
+    //               cs!("dbus-launch"),
+    //               cs!("Hyprland"), cs!("--i-am-really-stupid"),
+    //               //cs!("weston"),
+    //               std::ptr::null::<*const i8>());
+    //        _exit(EXIT_FAILURE);
+    //    }
+    //    assert_ne!(winsys_pid, -1);
+    //
+    //    let iwd_pid = fork();
+    //    if iwd_pid == 0 {
+    //        sigprocmask(SIG_SETMASK, orig_sig, std::ptr::null_mut());
+    //        execlp(cs!("/usr/libexec/iwd"), cs!("/usr/libexec/iwd"),
+    //               std::ptr::null::<*const i8>());
+    //        _exit(EXIT_FAILURE);
+    //    }
+    //    assert_ne!(iwd_pid, -1);
 
     if true_init {
         assert_eq!(system(cs!("dhcpcd eth0")), 0);
@@ -118,7 +132,11 @@ unsafe fn run_spawner(orig_sig : *const sigset_t, true_init: bool) -> std::io::R
     if sh_pid == 0 {
         sigprocmask(SIG_SETMASK, orig_sig, std::ptr::null_mut());
 
-        execlp(cs!("/bin/sh"), cs!("/bin/sh"), std::ptr::null() as *const c_char);
+        execlp(
+            cs!("/bin/sh"),
+            cs!("/bin/sh"),
+            std::ptr::null() as *const c_char,
+        );
         _exit(EXIT_FAILURE);
     }
     assert_ne!(sh_pid, -1);
@@ -130,7 +148,6 @@ unsafe fn run_spawner(orig_sig : *const sigset_t, true_init: bool) -> std::io::R
 
     _exit(EXIT_SUCCESS);
 }
-
 
 fn main() -> std::io::Result<()> {
     unsafe {
@@ -148,9 +165,21 @@ fn main() -> std::io::Result<()> {
             setenv(cs!("HOME"), cs!("/root"), 1);
             setenv(cs!("XDG_RUNTIME_DIR"), cs!("/run/user/1"), 1);
 
-            assert_eq!(mount(cs!("none"), cs!("/proc"), cs!("proc"), 0, std::ptr::null()), 0);
-            assert_eq!(mount(cs!("none"), cs!("/sys"), cs!("sysfs"), 0, std::ptr::null()), 0);
-            mount(cs!("none"), cs!("/dev"), cs!("devtmpfs"), 0, std::ptr::null());
+            assert_eq!(
+                mount(cs!("none"), cs!("/proc"), cs!("proc"), 0, std::ptr::null()),
+                0
+            );
+            assert_eq!(
+                mount(cs!("none"), cs!("/sys"), cs!("sysfs"), 0, std::ptr::null()),
+                0
+            );
+            mount(
+                cs!("none"),
+                cs!("/dev"),
+                cs!("devtmpfs"),
+                0,
+                std::ptr::null(),
+            );
 
             mkdir(cs!("/dev/pts"), 0o0777);
             mkdir(cs!("/dev/shm"), 0o0777);
@@ -159,8 +188,26 @@ fn main() -> std::io::Result<()> {
             mkdir(cs!("/run/user"), 0o0777);
             mkdir(cs!("/run/user/1"), 0o0700);
 
-            assert_eq!(mount(cs!("none"), cs!("/dev/pts"), cs!("devpts"), 0, std::ptr::null()), 0);
-            assert_eq!(mount(cs!("none"), cs!("/dev/shm"), cs!("tmpfs"), 0, std::ptr::null()), 0);
+            assert_eq!(
+                mount(
+                    cs!("none"),
+                    cs!("/dev/pts"),
+                    cs!("devpts"),
+                    0,
+                    std::ptr::null()
+                ),
+                0
+            );
+            assert_eq!(
+                mount(
+                    cs!("none"),
+                    cs!("/dev/shm"),
+                    cs!("tmpfs"),
+                    0,
+                    std::ptr::null()
+                ),
+                0
+            );
 
             assert_eq!(system(cs!("ip l set lo up")), 0);
             assert_eq!(system(cs!("ip a add 127.0.0.1 dev lo")), 0);
@@ -181,21 +228,24 @@ fn main() -> std::io::Result<()> {
             match (*cur).si_signo {
                 SIGCHLD => {
                     let mut st = mem::MaybeUninit::<c_int>::uninit();
-                    if (*cur).si_code == CLD_EXITED || (*cur).si_code == CLD_KILLED || (*cur).si_code == CLD_DUMPED {
+                    if (*cur).si_code == CLD_EXITED
+                        || (*cur).si_code == CLD_KILLED
+                        || (*cur).si_code == CLD_DUMPED
+                    {
                         waitpid((*cur).si_pid(), st.as_mut_ptr(), 0);
                         if (*cur).si_pid() == spawner_pid {
                             break 'wait_children;
                         }
                     }
-                },
+                }
 
                 SIGUSR1 => {
                     break 'wait_children;
-                },
+                }
 
                 _ => {
                     println!("unknown sig");
-                },
+                }
             }
         }
         let mut spawner_st = 0;
@@ -223,8 +273,6 @@ fn main() -> std::io::Result<()> {
                 libc::sleep(1);
             }
         }
-
     }
     Ok(())
 }
-
--- tools/rinit/src/server.rs
+++ tools/rinit/src/server.rs
@@ -1,6 +1,5 @@
-use std::thread;
 use std::io::ErrorKind;
-
+use std::thread;
 
 fn send_404(rq: tiny_http::Request) {
     let res = tiny_http::Response::from_string("404").with_status_code(404);
@@ -11,10 +10,10 @@ fn handle_req(rq: tiny_http::Request) {
     let url = rq.url().to_string();
     if url == "/" {
         let html = r#"<html><head><meta charset="utf-8"/></head><body><p>Welcome to FireBox!!</p><p>ようこそ！</p></body></html>"#;
-        let res = tiny_http::Response::from_string(html.to_string())
-            .with_header(tiny_http::Header{
+        let res =
+            tiny_http::Response::from_string(html.to_string()).with_header(tiny_http::Header {
                 field: "Content-Type".parse().unwrap(),
-                value: "text/html".parse().unwrap()
+                value: "text/html".parse().unwrap(),
             });
         let _ = rq.respond(res);
     } else {
@@ -26,7 +25,7 @@ pub fn start_server() -> std::io::Result<()> {
     let server = tiny_http::Server::http("127.0.0.1:8080");
 
     if let Ok(server) = server {
-        thread::spawn( move || {
+        thread::spawn(move || {
             for rq in server.incoming_requests() {
                 handle_req(rq);
             }
@@ -34,7 +33,6 @@ pub fn start_server() -> std::io::Result<()> {
     } else {
         return Err(std::io::Error::new(ErrorKind::Other, "oh!"));
     }
-
 
     Ok(())
 }
